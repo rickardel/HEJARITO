0849f16579387e9d291cecf22f64500e7d450b53
rickard el <rickardel@hotmail.com>
2018-03-07 10:23:56 +0100

    Init

---
+++ HEJARITO/HEJARITO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HEJARITO.Startup))]
+namespace HEJARITO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
