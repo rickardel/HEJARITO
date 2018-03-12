a632c1eaaeaa17d1122888804fb280e9291c452a
Henrik Gahn <henrik.gahn@outlook.com>
2018-03-12 14:06:32 +0100

    Bootstrap navbar, footer

--- HEJARITO/HEJARITO/App_Start/BundleConfig.cs
+++ HEJARITO/HEJARITO/App_Start/BundleConfig.cs
@@ -16,16 +16,17 @@ namespace HEJARITO
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
