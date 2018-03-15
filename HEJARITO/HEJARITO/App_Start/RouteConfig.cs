103606238ced424041568ddc614cd4bdf5d25a25
Tomas Mayer <tomas.mayer.ur@gmail.com>
2018-03-15 15:04:19 +0100

    Svenska texter isf engelska i GUI. Man hamnar på sin rollberoende startvy när man väljer 'Tillbaka till startsidan' i en CRUD-vy eller är inloggad redan vid start. 'Hem'-knappen är tillbaka i två varianter: 'Lärarhem' resp. 'Elevhem'.

--- HEJARITO/HEJARITO/App_Start/RouteConfig.cs
+++ HEJARITO/HEJARITO/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HEJARITO
             routes.MapRoute(
                 name: "LoginAsDefault",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "SelectStartView", id = UrlParameter.Optional }
             );
 
             //routes.MapRoute(
