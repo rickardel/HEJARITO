7ba6819354f30e3a8f9a24f9f9ac947ba3fa38cf
rickard el <rickardel@hotmail.com>
2018-03-23 07:47:45 +0100

    retouches + jobbar på custom exceptionfilter

---
+++ HEJARITO/HEJARITO/Models/HejaRiToExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HEJARITO.Models
+{
+    public class HejaRiToExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                filterContext.Result = new JsonResult
+                {
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                    Data = new
+                    {
+                        Message = "Något gick fel med Ajax anrop, Pröva igen senare.",
+                    }
+                };
+            }
+            filterContext.HttpContext.Response.StatusCode = 500;
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
